3e70e2e0d3f60b3e34a3d640f63a4f9e0bf75283
abigailNottingham <abigailillustration@gmail.com>
2021-05-12 07:58:51 +0100

    ex 5.14 & 5.15 created unit tests for blog visibility when show button clicked and that like button responds with a func call for every click.

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,17 +1,25 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import Blog from "./Blog"
 
 describe("<Blog />", () => {
-  test("title and author rendered as default but url and likes hidden", () => {
-    const blog = {
-      title: "Test blog",
-      author: "Nobody",
-      url: "http",
-      likes: 5
-    }
+  const user = {
+    username: "Test",
+    name: "test",
+    id: "6086845191271009b65fa077"
+  }
+  const blog = {
+    title: "Test blog",
+    author: "Nobody",
+    url: "http",
+    likes: 5,
+    user: "6086845191271009b65fa077"
+  }
+  const addLike = jest.fn()
+  const removeBlog = jest.fn()
 
+  test("title and author rendered as default but url and likes hidden", () => {
     const component = render(
       <Blog blog={blog} />
     )
@@ -24,5 +32,32 @@ describe("<Blog />", () => {
     const likes = component.container.querySelector(".likes")
 
     expect(url && likes).toBeNull()
+  })
+
+  test("url and likes shown when button is clicked", () => {
+    const component = render(
+      <Blog blog={blog} addLike={addLike} removeBlog={removeBlog} user={user} />
+    )
+    const button = component.getByText("view")
+    fireEvent.click(button)
+
+    const url = component.container.querySelector(".url")
+    const likes = component.container.querySelector(".likes")
+
+    expect(url && likes).toBeDefined()
+  })
+
+  test("likes button responds to every click with function handle", () => {
+    const component = render(
+      <Blog blog={blog} addLike={addLike} removeBlog={removeBlog} user={user} />
+    )
+    const button = component.getByText("view")
+    fireEvent.click(button)
+
+    const likeButton = component.getByText("like")
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(addLike.mock.calls).toHaveLength(2)
   })
 })
